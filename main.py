044c2440a52ea5c86eef4259e391817185d6bee9
Davidgit <dwoji@protonmail.com>
2023-05-04 13:29:50 +0200

    Updated main.py; node_creator.py; adden new Nodes, SAME AS relation, fixed some bugs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
+import re
 from bs4 import BeautifulSoup
+from node_creator import NodeCreator
 
 # Set the URL and language code
 URL_language_eng = 'https://ects.coi.pw.edu.pl/menu2/changelang/lang/eng'
@@ -14,7 +16,26 @@ def soup_request(url):
 
     # extract language code from URL
     lang = url.split('/')[-1]
-    language = 'Polish' if lang == 'pl' else 'English'
+
+    lang_dict = {
+        'pl': {
+            'language': 'Polish',
+            'university': 'Politechnika Warszawska',
+            'country': 'Polska',
+            'city': 'Warszawa'
+        },
+        'eng': {
+            'language': 'English',
+            'university': 'Warsaw University of Technology',
+            'country': 'Poland',
+            'city': 'Warsaw'
+        }
+    }
+
+    language = lang_dict[lang]['language']
+    university = lang_dict[lang]['university']
+    country = lang_dict[lang]['country']
+    city = lang_dict[lang]['city']
 
     req = sess.get('https://ects.coi.pw.edu.pl/menu2/programy')
     print(req.status_code)
@@ -59,12 +80,20 @@ def soup_request(url):
 
                     # extracting all syllabus links
                     syllabus = tr.findChildren('td')[-1].find('a')['href'].strip()
-                    syllabus_link = f'https://ects.coi.pw.edu.pl{syllabus}'
+
+                    # regular expression pattern to match the needed links
+                    pattern = r"/menu3/view2/idPrzedmiot/\d+"
+
+                    # checking if the extracted link matches the pattern
+                    if re.match(pattern, syllabus):
+                        syllabus_link = f'https://ects.coi.pw.edu.pl{syllabus}'
+                    else:
+                        syllabus_link = None
 
                     if name != '∑=':
-                        subjects = {'University_name': 'Politechnika Warszawska',
-                                    'City': 'Warsaw',
-                                    'Country': 'POL',
+                        subjects = {'University_name': university,
+                                    'City': city,
+                                    'Country': country,
                                     'Source': url,
                                     'Language': language,
                                     'Subject': subject,
@@ -100,86 +129,34 @@ data_frames.append(df2)
 final_df = pd.concat(data_frames)
 final_df.to_csv('final_data.csv', index=False)
 
-print(final_df)
+
+# final_df2 = pd.read_csv('final_data.csv')  # delete after
+#
+# print(final_df2['Syllabus'].value_counts())  # delete
+
+
+
+
+
+# print(final_df)
 print(final_df.shape)
 
-# creating sets for unique nodes and relations
-unique_nodes = set()
-unique_relations = set()
-node_properties = []
-
-for _, row in final_df.iterrows():
-    # create university node
-    university_node = {
-        'Node_name': row['University_name'],
-        'Node_type': 'university',
-        'source': row['Source'],
-        'language': row['Language'],
-    }
-    # add university node to unique nodes set
-    unique_nodes.add(tuple(university_node.items()))
-
-    # create city node
-    city_node = {
-        'Node_name': row['City'],
-        'Node_type': 'city',
-        'source': row['City'],
-        'language': row['Language'],
-    }
-    # add city node to unique nodes set
-    unique_nodes.add(tuple(city_node.items()))
-
-    # create country node
-    country_node = {
-        'Node_name': row['Country'],
-        'Node_type': 'country',
-        'source': row['Country'],
-        'language': row['Language'],
-    }
-    # add country node to unique nodes set
-    unique_nodes.add(tuple(country_node.items()))
-
-    # create university-city relation
-    university_city_relation = {"source": row['University_name'],
-                                "label": "IS_LOCATED",
-                                "target": row['City']}
-    # add university-city relation to unique relations set
-    unique_relations.add(tuple(university_city_relation.items()))
-
-    # create city-country relation
-    city_country_relation = {"source": row['City'],
-                             "label": "IS_PART_OF",
-                             "target": row['Country']}
-    # add city-country relation to unique relations set
-    unique_relations.add(tuple(city_country_relation.items()))
-
-# Loop through the unique nodes
-for node in unique_nodes:
-
-    # Loop through the items in each node tuple
-    for item in node:
-        # Append the node property as a dictionary to the node_properties list
-        node_property = {
-            'node_name': 'node_name',
-            'property_name': 'property_name',
-            'property_value': 'property_value'
-        }
-node_properties.append(node_property)
 
 
-# convert unique nodes and relations sets back to dictionaries
-unique_nodes = [dict(node) for node in unique_nodes]
-unique_relations = [dict(relation) for relation in unique_relations]
+# creating an instance of NodeCreator with the final_df
+node_creator = NodeCreator(final_df)
 
-nodes_df = pd.DataFrame(unique_nodes)
-relations_df = pd.DataFrame(unique_relations)
+# calling the process_data method to process the data
+node_creator.process_data()
 
-nodes_df.to_csv('nodes.csv')
-relations_df.to_csv('relations.csv')
+# saving the processed data to CSV files
+node_creator.save_data_to_csv()
 
-# Convert the node_properties list to a DataFrame
-node_properties_df = pd.DataFrame(node_properties)
-node_properties_df.to_csv('node_properties.csv', index=False)
+
+
+
+
+
 
 
 
